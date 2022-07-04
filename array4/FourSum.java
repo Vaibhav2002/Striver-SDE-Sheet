dd77f9e33f8b2e995d0e9cf6c557e7fd83cf99c8
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-07-04 20:55:44 +0530

    Few Array 4 problems added

---
+++ array4/FourSum.java
@@ -0,0 +1,2 @@
+package array4;public class FourSome {
+}
---
+++ array4/TwoSum.java
@@ -0,0 +1,18 @@
+package array4;
+
+import java.util.HashMap;
+
+/**
+ * <a href="https://leetcode.com/problems/two-sum/">Problem</a>
+ **/
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        var map = new HashMap<Integer, Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target - nums[i]))
+                return new int[]{map.get(target - nums[i]), i};
+            map.put(nums[i], i);
+        }
+        return new int[2];
+    }
+}

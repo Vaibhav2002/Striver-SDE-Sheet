b85c4f09deefe398c071bd06c79f3288ab44dd32
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-07-04 20:57:01 +0530

    Few Array 4 problems added

--- array4/FourSum.java
+++ array4/FourSum.java
@@ -1,2 +1,37 @@
-package array4;public class FourSome {
+package array4;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class FourSum {
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 3; i++) {
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                var l = find(i, j, nums, target - nums[i] - nums[j]);
+                ans.addAll(l);
+                while(j + 1 < nums.length && nums[j + 1] == nums[j]) ++j;
+            }
+            while(i + 1 < nums.length && nums[i + 1] == nums[i]) ++i;
+        }
+        return ans;
+    }
+
+    private HashSet<List<Integer>> find(int a, int b, int[] nums, int target) {
+        HashSet<List<Integer>> ans = new HashSet<>();
+        int i = b + 1, j = nums.length - 1;
+        while (i < j) {
+            if (nums[i] + nums[j] == target) {
+                var l = List.of(nums[a], nums[b], nums[i], nums[j]);
+                ans.add(l);
+                while(i<j && nums[i] == l.get(2)) ++i;
+                while(i<j && nums[j] == l.get(3)) ++j;
+            }
+            else if(nums[i]+nums[j]<target) i++;
+            else j--;
+        }
+        return ans;
+    }
 }
--- array4/LongestSubstringWithoutRepetition.java
+++ array4/LongestSubstringWithoutRepetition.java
@@ -1,2 +1,26 @@
-package array4;public class LongestSubstringWithoutRepitition {
+package array4;
+
+import java.util.HashMap;
+
+/**
+* <a href="https://leetcode.com/problems/longest-substring-without-repeating-characters/">Problem</a>
+**/
+public class LongestSubstringWithoutRepetition {
+    public int lengthOfLongestSubstring(String s) {
+        var map = new HashMap<Character, Integer>();
+        int start = 0, end = 0;
+        int ans = 0;
+        char[] ch = s.toCharArray();
+        for(int i=0;i< ch.length;i++){
+            char c = ch[i];
+            if(map.containsKey(c)) {
+                ans = Math.max(ans, end - start);
+                start = map.get(c)+1;
+                map.remove(c);
+            }
+            map.put(c, i);
+            end++;
+        }
+        return ans;
+    }
 }

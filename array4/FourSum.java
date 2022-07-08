f9fdb9be10e555faad3a0c0704267e8fa1d362d9
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-07-08 17:15:51 +0530

    FourSum problem completed and New Array 4 problem added

--- array4/FourSum.java
+++ array4/FourSum.java
@@ -1,35 +1,36 @@
 package array4;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FourSum {
 
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 3; i++) {
-            for (int j = i + 1; j < nums.length - 2; j++) {
-                var l = find(i, j, nums, target - nums[i] - nums[j]);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                var l = find(i, j, nums, (long) target - nums[i] - nums[j]);
                 ans.addAll(l);
-                while(j + 1 < nums.length && nums[j + 1] == nums[j]) ++j;
+                while (j + 1 < nums.length && nums[j + 1] == nums[j]) ++j;
             }
-            while(i + 1 < nums.length && nums[i + 1] == nums[i]) ++i;
+            while (i + 1 < nums.length && nums[i + 1] == nums[i]) ++i;
         }
         return ans;
     }
 
-    private HashSet<List<Integer>> find(int a, int b, int[] nums, int target) {
-        HashSet<List<Integer>> ans = new HashSet<>();
+    private List<List<Integer>> find(int a, int b, int[] nums, long target) {
+        List<List<Integer>> ans = new ArrayList<>();
         int i = b + 1, j = nums.length - 1;
         while (i < j) {
-            if (nums[i] + nums[j] == target) {
+            long sum = (long) nums[i] + (long) nums[j];
+            if (sum == target) {
                 var l = List.of(nums[a], nums[b], nums[i], nums[j]);
                 ans.add(l);
-                while(i<j && nums[i] == l.get(2)) ++i;
-                while(i<j && nums[j] == l.get(3)) ++j;
-            }
-            else if(nums[i]+nums[j]<target) i++;
+                while (i < j && nums[i] == l.get(2)) ++i;
+                while (i < j && nums[j] == l.get(3)) --j;
+            } else if (sum < target) i++;
             else j--;
         }
         return ans;
---
+++ array4/LongestConsecutiveSequence.java
@@ -0,0 +1,24 @@
+package array4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.com/problems/longest-consecutive-sequence/">Problem</a>
+ **/
+public class LongestConsecutiveSequence {
+    public int longestConsecutive(int[] nums) {
+        var set = new HashSet<Integer>();
+        for (int i : nums) set.add(i);
+        int ans = 0;
+        for (int i : nums) {
+            if (set.contains(i - 1)) continue;
+            int j = i;
+            while (set.contains(j)) j++;
+            ans = Math.max(ans, j - i);
+        }
+        return ans;
+    }
+}

58646e851dd976c5d860ac597dd97fa81438021a
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-08-26 20:51:25 +0530

    Flatten Tree few solutions added

--- binaryTree3/FlattenTreeIntoLinkedList.java
+++ binaryTree3/FlattenTreeIntoLinkedList.java
@@ -4,25 +4,52 @@ import binaryTree1.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
 * <a href="https://leetcode.com/problems/flatten-binary-tree-to-linked-list/">Problem</a>
 **/
 public class FlattenTreeIntoLinkedList {
+
+    private TreeNode prev = null;
     public void flatten(TreeNode root) {
-        List<TreeNode> pre = new ArrayList<>();
-        getPreOrder(root, pre);
-        for(int i = 1;i<pre.size();i++){
-            root.right = pre.get(i);
-            root.left = null;
-            root = root.right;
+        recur(root);
+    }
+
+    private void recur(TreeNode root){
+        if(root == null) return;
+        recur(root.right);
+        recur(root.left);
+        root.left = null;
+        root.right = prev;
+        prev = root;
+    }
+
+    private void iterative(TreeNode root){
+        if(root == null) return;
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()){
+            TreeNode cur = st.peek();
+            if(cur.right!=null) st.push(cur.right);
+            if(cur.left!=null) st.push(cur.left);
+            cur.right = st.peek();
+            cur.left = null;
         }
     }
 
-    private void getPreOrder(TreeNode root, List<TreeNode> pre){
+    private void morris(TreeNode root){
         if(root == null) return;
-        pre.add(root);
-        getPreOrder(root.left, pre);
-        getPreOrder(root.right, pre);
+        TreeNode cur = root;
+        while(cur !=null){
+            if(cur.left !=null){
+                TreeNode inLast = cur.left;
+                while(inLast.right!=null) inLast = inLast.right;
+                inLast.right = cur.right;
+                cur.right = cur.left;
+                cur.left = null;
+            }
+            cur = cur.right;
+        }
     }
 }

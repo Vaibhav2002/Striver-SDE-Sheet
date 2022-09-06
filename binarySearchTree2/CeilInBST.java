51ca07af690f97b3177548ec8e5fbe894d03e0e7
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-09-06 21:00:17 +0530

    Few Binary Search Tree 2 solutions added

--- binarySearchTree2/CeilInBST.java
+++ binarySearchTree2/CeilInBST.java
@@ -1,17 +1,20 @@
 package binarySearchTree2;
 
+/**
+ * <a href="https://www.codingninjas.com/codestudio/problems/ceil-from-bst_920464">Problem</a>
+ **/
 public class CeilInBST {
     private static TreeNode<Integer> ceil;
 
     public static int findCeil(TreeNode<Integer> root, int X) {
-        ceil = null;
+        ceil = new TreeNode<>(-1);
         recur(root, X);
         return ceil.data;
     }
 
     private static int iterative(TreeNode<Integer> root, int x) {
         TreeNode<Integer> temp = root;
-        int ans = 0;
+        int ans = -1;
         while (temp != null) {
             if (temp.data == x) return x;
             if (temp.data > x) {
